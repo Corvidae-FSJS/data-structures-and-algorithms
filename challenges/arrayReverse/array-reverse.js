cbd31062ecf5d7b8da9e86b4702c3c0cda8e2cc0
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-17 16:50:04 -0700

    travis won't pass because there is an imaginary linting error in my comments

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -3,11 +3,11 @@ const array2 = [89, 2354, 3546, 23, 10, -923, 823, -12];
 const array3 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 
 function reverseArray(arr) {
-  newArr = [];
-  for(i = 0; i < arr.length; i++) {
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++) {
     newArr[i] = arr[arr.length - (i + 1)];
   }
-  return newArr
+  return newArr;
 }
 
 console.log(reverseArray(array1));
@@ -27,7 +27,7 @@ function reverse(arr) {
 }
 
 
-MORE PREFERRED  USES LESS MEMORY, ENERGY
+MORE PREFERRED USES LESS MEMORY, ENERGY
 function reverse2(arr) {
   for(let i =0; i < arr.length/2; i++) {
     const temp = arr[i];
