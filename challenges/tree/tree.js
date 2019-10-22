afd60521e4dcce1e5342d87f5eded54fbead3f25
Dylan Corvidae <dylancorvidae@gmail.com>
2019-10-21 19:45:42 -0700

    created node and binary tree classes

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -11,4 +11,80 @@ class BinaryTree {
     this.root = null;
     this.traversalArray = [];
   }
+
+  preOrder(node) {
+    if(node) {
+      this.traversalArray.push(node.value);
+      this.preOrder(node.left);
+      this.preOrder(node.right);
+    }
+    return this.traversalArray;
+  }
+
+  inOrder(node) {
+    if(node) {
+      this.preOrder(node.left);
+      this.traversalArray.push(node.value);
+      this.preOrder(node.right);
+    }
+    return this.traversalArray;
+  }
+
+  postOrder(node) {
+    if(node) {
+      this.preOrder(node.left);
+      this.preOrder(node.right);
+      this.traversalArray.push(node.value);
+    }
+    return this.traversalArray;
+  }
+
+  add(value) {
+    let newNode = new Node(value);
+    if(this.root === null) {
+      this.root = newNode;
+    }
+    else {
+      this.addNode(this.root, newNode);
+    }
+  }
+
+  addnode(node, newNode) {
+    if(newNode.value < node.value) {
+      if(node.left === null) {
+        node.left = newNode;
+      }
+      else {
+        this.addNode(node.left, newNode);
+      }
+    }
+    else {
+      if(node.right === null) {
+        node.right = newNode;
+      }
+      else {
+        this.addNode(node.right, newNode);
+      }
+    }
+  }
+
+  contains(value) {
+    let has = false;
+    if(this.value === value) {
+      has = true;
+    }
+    else if(value.left !== null && value < this.value) {
+      this.add(this.left);
+    }
+    else if(value.right !== null && value > this.value) {
+      this.add(this.right);
+    }
+    this.add(this);
+    return has;
+  }
 }
+
+module.exports = {
+  Node,
+  BinarTree,
+};
