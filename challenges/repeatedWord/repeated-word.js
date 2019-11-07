cad4fa34bb910754cc64ca025c61166692607d02
Dylan Corvidae <dylancorvidae@gmail.com>
2019-11-07 15:10:10 -0800

    built function

--- challenges/repeatedWord/repeated-word.js
+++ challenges/repeatedWord/repeated-word.js
@@ -1,3 +1,17 @@
-const repeatedWord = (word) => {
-  
-}
+const repeatedWord = (book) => {
+  const noPunctuation = book.replace(/[^\w\s]/, '');
+  const wordList = noPunctuation.toLowerCase().split(' ');
+  let wordCount = {};
+  let answer;
+
+  for(let i = 0; i < wordList.length; i++) {
+    if(!wordCount[wordList[i]]) {
+      wordCount[wordList[i]] = 1;
+    } else {
+      answer = wordList[i];
+    }
+  }
+  return answer;
+};
+
+module.exports = repeatedWord;
